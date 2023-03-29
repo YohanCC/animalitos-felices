6ade4fce28a766030c35338587c9c2d9de5c4ddc
Yohan CC <acuario.kaki@gmail.com>
2023-03-29 13:10:41 -0400

    Quiero introducir el Javascript a mi GitHub

---
+++ Javascript/ppt.js
@@ -0,0 +1,17 @@
+var computadora 
+var celular
+var tablet
+
+
+
+function solution (article) {
+    if (article == computadora) {
+        console.log("compraste una computadora")
+    } else if(article == celular) {
+        console.log("commpraste un celular")
+    } else if(arccle == tablet) {
+        console.log("si compraste una tablet, loco")
+    } else {
+        console.log("no compraste nada :(")
+    }
+}
